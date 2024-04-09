23042883d001f78ffd4f5a67cb641af55cb61ee7
GorbunovGHub <pavlixrus@mail.ru>
2024-04-09 15:37:09 +0300

    Homework6.1
    Списки и словари

--- Homework6.py
+++ Homework6.py
@@ -2,7 +2,7 @@ my_list = ["apple", "coconut", "arange", "banana", "peach"]
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
-print(my_list[3:5])
+print(my_list[2:5])
 my_list[3] = "Pear"
 print(my_list)
 
